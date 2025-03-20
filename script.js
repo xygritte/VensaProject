438752d96c20d2271dfaa394650c67ea26b127e3
Furqon Ramadhani <ahfura70@gmail.com>
2025-03-20 23:34:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    alert('Thank you for your message!');
+});
